35c24139922a6a558652030cb595060cdd50e082
Erich Dylus <51535743+ErichDylus@users.noreply.github.com>
2024-11-01 14:28:40 -0400

    Update MsgSenderSpecificCreate2Deployer.sol

--- contracts/MsgSenderSpecificCreate2Deployer.sol
+++ contracts/MsgSenderSpecificCreate2Deployer.sol
@@ -2,44 +2,47 @@
 
 pragma solidity 0.8.18;
 
-/*
- *
- * @notice CREATE2 contract deployer for predictable addresses based on the `msg.sender` and their passed `salt`
- *
- * @dev replace `Contract` with the contract to be CREATE2-deployed, and adjust functions for any constructor params
- *
- **/
-
+/// @dev replace `Contract` with the contract to be CREATE2-deployed, and adjust functions for any constructor params
 contract Contract {
     constructor() payable {}
 }
 
-/// @title Msg.Sender-Specific Create2 Deployer
+/*
+ *  @title Msg.Sender-Specific Create2 Deployer
+ *
+ *  @notice CREATE2 contract deployer for predictable addresses based on the `msg.sender` and their passed `salt`
+ *
+ **/
 contract MsgSenderSpecificCreate2Deployer {
-    event Deployment(address newContract);
+    event Deployment(address deployer, address newContract);
 
     /// @notice deploys a new `Contract` via CREATE2
     /// @param salt uint256 which will be packed with `msg.sender` for the CREATE2 `salt` value
+    /// @return address contract address of the newly deployed `Contract`
     function deploy(uint256 salt) external returns (address) {
         bytes32 _salt = keccak256(abi.encodePacked(msg.sender, salt));
         Contract _contract = new Contract{salt: _salt, value: 0}();
         address _newContract = address(_contract);
 
-        emit Deployment(_newContract);
+        emit Deployment(msg.sender, _newContract);
         return _newContract;
     }
 
     /// @notice returns the predicted new contract address for `Contract` if `msg.sender` calls `deploy()` passing `salt`
     /// @param salt uint256 which will be packed with `msg.sender` for the CREATE2 `salt` value
+    /// @return address predicted contract address of the prospective `Contract`
     function getAddress(uint256 salt) public view returns (address) {
         bytes32 _salt = keccak256(abi.encodePacked(msg.sender, salt));
-        bytes32 hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(getBytecode())));
-        return address(uint160(uint256(hash)));
+        bytes32 _hash = keccak256(abi.encodePacked(bytes1(0xff), address(this), _salt, keccak256(getBytecode())));
+        
+        return address(uint160(uint256(_hash)));
     }
 
-    /// @notice returns the creation bytecode for `Contract`
+    /// @notice getter for the the creation bytecode for `Contract`
+    /// @return bytes creation bytecode for `Contract`
     function getBytecode() public pure returns (bytes memory) {
         bytes memory bytecode = type(Contract).creationCode;
+        
         return abi.encodePacked(bytecode);
     }
 }
